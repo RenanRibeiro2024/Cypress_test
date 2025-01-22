57dc7553f5b65c23a1a410d08679d860ed4207ad
Renan Ribeiro <ribeirorenan294@yahoo.com.br>
2025-01-22 14:26:20 -0300

    Jireh teste

--- cypress/e2e/busca-google.cy.js
+++ cypress/e2e/busca-google.cy.js
@@ -1,14 +1,10 @@
-describe('template spec', () => {
+describe('Busca Google', () => {
   it('passes', () => {
     cy.visit('https://google.com.br')
-  })
 
-  /* ==== Test Created with Cypress Studio ==== */
-  it('SejuPRO', function() {
     /* ==== Generated with Cypress Studio ==== */
-    cy.visit('https://google.com');
-    cy.get('.FPdoLc > center > .gNO89b').click();
-    /* ==== End Cypress Studio ==== */
-  });
+    cy.get('#APjFqb').type('enem 2025');
+
+  })
 })
 
---
+++ cypress/e2e/jireh.cy.js
@@ -0,0 +1,9 @@
+describe('template spec', () => {
+  it('passes', () => {
+    cy.visit('https://www.google.com');
+    /* ==== End Cypress Studio ==== */
+    /* ==== Generated with Cypress Studio ==== */
+    cy.get('#APjFqb').click();
+    /* ==== End Cypress Studio ==== */
+  })
+})
