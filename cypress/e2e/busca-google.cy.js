0a0cc19bc9c1022ecfa8f5f460c463236ccc3cba
Renan Ribeiro <ribeirorenan294@yahoo.com.br>
2025-01-22 14:24:10 -0300

    Jireh Faturamento

---
+++ cypress/e2e/busca-google.cy.js
@@ -0,0 +1,14 @@
+describe('template spec', () => {
+  it('passes', () => {
+    cy.visit('https://google.com.br')
+  })
+
+  /* ==== Test Created with Cypress Studio ==== */
+  it('SejuPRO', function() {
+    /* ==== Generated with Cypress Studio ==== */
+    cy.visit('https://google.com');
+    cy.get('.FPdoLc > center > .gNO89b').click();
+    /* ==== End Cypress Studio ==== */
+  });
+})
+
